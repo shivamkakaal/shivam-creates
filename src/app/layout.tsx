21ad3c43eda0b5ccd6e7fad88ec3a3fd49da16e9
shivamkakaal <shivamkakaal@gmail.com>
2026-05-14 08:23:51 +0530

    Configure Favicons and Web Manifests

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,17 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: [
+      { url: '/images/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/images/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    shortcut: '/images/favicon.ico',
+    apple: [
+      { url: '/images/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/images/site.webmanifest',
 };
 
 export const viewport: Viewport = {
